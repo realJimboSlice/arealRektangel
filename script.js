bfd2e44c8c0cea529a58429d5e2388b5388d5508
realJimboSlice <jimm7493@stud.kea.dk>
2024-02-01 21:03:56 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function udregnRektangelAreal(width, height) {
+  let areal = width * height;
+  return areal;
+}
+
+console.log(
+  "Funktionen virker og dette er arealet:",
+  udregnRektangelAreal(10, 20)
+);
